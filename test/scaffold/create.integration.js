d576d1ac3226ac0df88a32a44d94d1523feb2302
Ivan Shumkov <ivan@shumkov.ru>
2022-09-19 16:20:54 +0400

    test: add data dir check into tests

--- test/scaffold/create.integration.js
+++ test/scaffold/create.integration.js
@@ -65,9 +65,11 @@ describe('#create', function() {
 
       var configPath = testDir + '/mynode/dashcore-node.json';
       var packagePath = testDir + '/mynode/package.json';
+      var dataPath = testDir + '/mynode/data';
 
       should.equal(fs.existsSync(configPath), true);
       should.equal(fs.existsSync(packagePath), true);
+      should.equal(fs.existsSync(dataPath), true);
 
       var config = JSON.parse(fs.readFileSync(configPath));
       config.services.should.deep.equal(['dashd', 'web']);
